d188cac5c7cb2025513b1e7f58a49ec81f50e4ed
Icaro Oliveira <i.oliveira@coblue.com.br>
2026-03-19 18:35:37 -0300

    refactor: alter column

--- database/migrations/2025_08_17_185945_add_end_conversation_time_to_transcripts_table.php
+++ database/migrations/2025_08_17_185945_add_end_conversation_time_to_transcripts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('transcripts', function (Blueprint $table) {
-            $table->bigInteger('end_conversation_time')->nullable()->after('conversation');
+            $table->integer('end_conversation_time')->nullable()->after('conversation');
         });
     }
 
